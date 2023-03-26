6102b7fadbcb5774f9201600bcf6c92880dffca4
Baila3 <bailaly16@gmail.com>
2023-03-25 23:32:48 -0400

    add scoreboard

--- birdflaps/Assets/LogicScript.cs
+++ birdflaps/Assets/LogicScript.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogicScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int playerScore;
+    public Text scoreText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
