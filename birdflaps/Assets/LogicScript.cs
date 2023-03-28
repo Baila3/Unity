97c68bcddc5ddaec668436b4d66c155b1bb0624e
Baila3 <bailaly16@gmail.com>
2023-03-27 22:44:05 -0400

    increase score

--- birdflaps/Assets/LogicScript.cs
+++ birdflaps/Assets/LogicScript.cs
@@ -7,6 +7,11 @@ public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
-    public int BigScore;
 
+    [ContextMenu("Increase Score")]
+    public void addScore()
+    {
+        playerScore += 1;
+        scoreText.text = playerScore.ToString();
+    }
 }
