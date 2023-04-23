08e6716674d5ac99574d3e7043c4608afae7810d
Baila3 <bailaly16@gmail.com>
2023-04-22 23:18:42 -0400

    mutiply function call

--- birdflaps/Assets/PipeMiddleScript.cs
+++ birdflaps/Assets/PipeMiddleScript.cs
@@ -23,6 +23,7 @@ public class PipeMiddleScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         logic.addScore();
+        logic.addScore();
         
     }
 
